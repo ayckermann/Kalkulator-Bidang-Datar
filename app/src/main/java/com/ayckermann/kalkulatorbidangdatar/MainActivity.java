93273793c907ae6c58b6488ac9e7ad1de9a330ae
ayckermann <aya.mf12@gmail.com>
2022-10-15 12:34:35 +0700

    algoritma done - need more decoration

--- app/src/main/java/com/ayckermann/kalkulatorbidangdatar/MainActivity.java
+++ app/src/main/java/com/ayckermann/kalkulatorbidangdatar/MainActivity.java
@@ -1,14 +1,52 @@
 package com.ayckermann.kalkulatorbidangdatar;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    public Button persegi, segitiga, lingkaran;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        persegi = findViewById(R.id.btnPersegi);
+        segitiga = findViewById(R.id.btnLSegitiga);
+        lingkaran = findViewById(R.id.btnLingkaran);
+
+        persegi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                replaceFragment(new Persegi());
+            }
+        });
+        segitiga.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                replaceFragment(new Segitiga());
+            }
+        });
+        lingkaran.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                replaceFragment(new Lingkaran());
+            }
+        });
+        
     }
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frameLayout, fragment);
+        fragmentTransaction.commit();
+    }
+
+
 }
